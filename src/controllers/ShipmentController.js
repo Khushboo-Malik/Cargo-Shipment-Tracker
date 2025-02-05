e7ec02d41f23c5022531fe76c706825d89d6e226
Khushboo Malik <mail.khushboomalik@gmail.com>
2025-02-05 13:08:16 +0530

    Added APIs to update location and create new shipment details

--- src/controllers/ShipmentController.js
+++ src/controllers/ShipmentController.js
@@ -36,6 +36,91 @@ async function fetchShipment(req, res) {
     }
 }
 
+async function createShipment(req, res) {
+    try {
+        const {
+            shipmentId,
+            containerId,
+            route,
+            currentLocation,
+            eta,
+            status,
+            origin,
+            destination,
+            departureTime,
+            transportMode,
+            alerts
+        } = req.body;
+
+        const existingShipment = await Shipment.findOne({ shipmentId });
+        if (existingShipment) {
+            return res.status(400).json({ message: "Shipment with this ID already exists" });
+        }
+
+        const newShipment = await Shipment.create({
+            shipmentId,
+            containerId,
+            route,
+            currentLocation,
+            eta,
+            status,
+            origin,
+            destination,
+            departureTime,
+            transportMode,
+            alerts
+        });
+
+        res.status(201).json({
+            message: "Shipment created successfully",
+            shipment: newShipment
+        });
+
+    } catch (error) {
+        console.error("Error in creating shipment:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+async function updateShipmentLocation(req, res) {
+    try {
+        const id = req.params.shipmentId; 
+        const { locationName, latitude, longitude } = req.body;
+
+
+        if (!locationName || latitude === undefined || longitude === undefined) {
+            return res.status(400).json({ message: "Please provide all location details" });
+        }
+
+        const shipment = await Shipment.findByIdAndUpdate(
+            id,
+            {
+                $set: {
+                    "currentLocation.locationName": locationName,
+                    "currentLocation.latitude": latitude,
+                    "currentLocation.longitude": longitude,
+                    "currentLocation.updatedAt": Date.now(),
+                }
+            },
+            { new: true }
+        );
+
+        if (!shipment) {
+            return res.status(404).json({ message: "Shipment not found" });
+        }
+
+        res.status(200).json({
+            message: "Shipment location updated successfully",
+            shipment
+        });
+
+    } catch (error) {
+        console.error("Error updating shipment location:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+
 module.exports={
-    fetchAllShipments,fetchShipment
+    fetchAllShipments,fetchShipment,createShipment,updateShipmentLocation
 };
--- src/routes/ShipmentRoutes.js
+++ src/routes/ShipmentRoutes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 const fetchAllShipments= require("../controllers/ShipmentController.js").fetchAllShipments;
 const fetchShipment= require("../controllers/ShipmentController.js").fetchShipment;
+const createShipment= require("../controllers/ShipmentController.js").createShipment;
+const updateShipmentLocation= require("../controllers/ShipmentController.js").updateShipmentLocation;
 
 
 router.get("/shipments",fetchAllShipments);
 router.get("/shipment/:shipmentId",fetchShipment);
+router.post("/shipment",createShipment);
+router.patch("/shipment/:shipmentId/update-location",updateShipmentLocation);
 
 
 
